97f80a4f336820e5e1986ff77af6f113ea455efc
Robson Cruz <ghost0092@gmail.com>
2020-12-03 22:31:04 -0300

    feat: add solution for day 02

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from advent_of_code.solutions import day_01
+from advent_of_code.solutions import day_02
 
 
 def main() -> None:
     print("Solving Day 01 challenge...")
     day_01.solve_day01()
+    print("Solving Day 02 challenge...")
+    day_02.solve_day02()
 
 
 if __name__ == "__main__":
