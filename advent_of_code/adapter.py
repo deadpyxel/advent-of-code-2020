edfadfff5aa1f6a8652cf4bb1f8029d3fe721928
Robson Cruz <ghost0092@gmail.com>
2020-12-03 21:10:14 -0300

    style: reformatting with black + flake8

--- advent_of_code/adapter.py
+++ advent_of_code/adapter.py
@@ -1,6 +1,8 @@
 import requests
 from requests.exceptions import HTTPError
-from .config import BASE_URL, AUTH_TOKEN
+
+from .config import AUTH_TOKEN
+from .config import BASE_URL
 
 
 def acquire_problem_input() -> str:
--- advent_of_code/solutions/day_01.py
+++ advent_of_code/solutions/day_01.py
@@ -1,17 +1,22 @@
-from typing import List, Sequence
 from itertools import combinations
+from typing import Iterable
+from typing import List
 
 from advent_of_code.adapter import acquire_problem_input
 from advent_of_code.utils import _parse_input_as_integer
 
 
-def filter_possible_combinations(
-    original_list: List[int], n: int, size: int = 2
-) -> List[Sequence[int]]:
-    return [pair for pair in combinations(original_list, r=size) if sum(pair) == n]
+def _filter_possible_combinations(
+    original_list: List[int], n: int, combination_size: int = 2
+) -> List[Iterable[int]]:
+    return [
+        comb
+        for comb in combinations(original_list, r=combination_size)
+        if sum(comb) == n
+    ]
 
 
-def calc_product(input_list: Sequence[int]) -> int:
+def _calc_product(input_list: Iterable[int]) -> int:
     result = 1
     for x in input_list:
         result *= x
@@ -24,11 +29,13 @@ def solve_day01() -> None:
 
     # Part 1 - We were asked to find product of a pair from the input
     # that has a sum of 2020
-    possible_values = filter_possible_combinations(input_as_integers, n=2020)
+    possible_values = _filter_possible_combinations(input_as_integers, n=2020)
     for possibility in possible_values:
-        print(f"Possible answer for part1: {calc_product(possibility)}")
+        print(f"Possible answer for part1: {_calc_product(possibility)}")
 
     # Part 2 - We were asked the prompt, but now with a triplet
-    possible_values = filter_possible_combinations(input_as_integers, n=2020, size=3)
+    possible_values = _filter_possible_combinations(
+        input_as_integers, n=2020, combination_size=3
+    )
     for possibility in possible_values:
-        print(f"Possible answer for part2: {calc_product(possibility)}")
+        print(f"Possible answer for part2: {_calc_product(possibility)}")
