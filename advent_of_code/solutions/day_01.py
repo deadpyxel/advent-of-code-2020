a9122d840c2a072aaed636c866b64bc24688737a
Robson Cruz <ghost0092@gmail.com>
2020-12-03 23:10:13 -0300

    feat: add solution for day 03

--- advent_of_code/solutions/day_01.py
+++ advent_of_code/solutions/day_01.py
@@ -3,7 +3,8 @@ from typing import Iterable
 from typing import List
 
 from advent_of_code.adapter import acquire_problem_input
-from advent_of_code.utils import _parse_input_as_integer
+from advent_of_code.utils import calc_product
+from advent_of_code.utils import parse_input_as_integer
 
 
 def _filter_possible_combinations(
@@ -16,26 +17,19 @@ def _filter_possible_combinations(
     ]
 
 
-def _calc_product(input_list: Iterable[int]) -> int:
-    result = 1
-    for x in input_list:
-        result *= x
-    return result
-
-
 def solve_day01() -> None:
     problem_input = acquire_problem_input()
-    input_as_integers = _parse_input_as_integer(problem_input)
+    input_as_integers = parse_input_as_integer(problem_input)
 
     # Part 1 - We were asked to find product of a pair from the input
     # that has a sum of 2020
     possible_values = _filter_possible_combinations(input_as_integers, n=2020)
     for possibility in possible_values:
-        print(f"Possible answer for part1: {_calc_product(possibility)}")
+        print(f"Possible answer for part1: {calc_product(possibility)}")
 
     # Part 2 - We were asked the prompt, but now with a triplet
     possible_values = _filter_possible_combinations(
         input_as_integers, n=2020, combination_size=3
     )
     for possibility in possible_values:
-        print(f"Possible answer for part2: {_calc_product(possibility)}")
+        print(f"Possible answer for part2: {calc_product(possibility)}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from advent_of_code.solutions import day_01
 from advent_of_code.solutions import day_02
+from advent_of_code.solutions import day_03
 
 
 def main() -> None:
@@ -7,6 +8,8 @@ def main() -> None:
     day_01.solve_day01()
     print("Solving Day 02 challenge...")
     day_02.solve_day02()
+    print("Solving Day 03 challenge...")
+    day_03.solve_day03()
 
 
 if __name__ == "__main__":
